dbf1dd28da393ca924194e345f1092e906e2c1c5
Nikola Ticojevic <nikola.ticojevic@interprefy.com>
2022-10-26 11:03:46 +0200

    Initial Commit

---
+++ TestApiDetailDisplay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestApiDetailDisplay
+//
+//  Created by Nikola Ticojevic on 10/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
